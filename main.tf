1dfb78adf9598b7ff256be743c6ca85ad4d33c77
Muhammad-Aamir1 <amir123344irfan@gmail.com>
2025-09-26 20:10:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
-resource "google_storage_bucket" "my-bucket" {
-  name                     = "tt-githubdemo-bucket-00231"
+resource "google_storage_bucket" "my-bucket121" {
+  name                     = "tt-githubdemo-bucket-bond"
   project                  = "project-4-endgame"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
 }
 #
-resource "google_storage_bucket" "my-bucket2" {
-  name                     = "tt-githubdemo-bucket-00212"
+resource "google_storage_bucket" "my-bucket2222" {
+  name                     = "tt-githubdemo-bucket-james"
   project                  = "project-4-endgame"
   location                 = "US"
   force_destroy            = true
