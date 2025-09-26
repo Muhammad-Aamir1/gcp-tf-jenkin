a6ac0207b748ecbb0fcb5670276ce25c6ef239f9
Muhammad-Aamir1 <amir123344irfan@gmail.com>
2025-09-26 20:23:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,5 +13,15 @@ resource "google_storage_bucket" "my-bucket2222" {
   force_destroy            = true
   public_access_prevention = "enforced"
 }
+resource "google_storage_bucket" "my-bucket-doomsday" {
+  name                     = "tt-githubdemo-bucket-victor"
+  project                  = "project-4-endgame"
+  location                 = "US"
+  force_destroy            = true
+  public_access_prevention = "enforced"
+}
+
+
+
 #
 ### dummy new
